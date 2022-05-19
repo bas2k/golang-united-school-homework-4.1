8def1e9cc263ea0740ead8c93588e7f27f98ed26
Alexander Brovikov <alexander.brovikov@waveaccess.global>
2022-05-20 02:40:08 +0500

    add solution

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bas2k/golang-united-school-homework-4.1
+
+go 1.15
--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,10 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+	runes := []rune(input)
+	output_runes := make([]rune, len(runes))
+	for i, r := range runes {
+		output_runes[len(runes)-i-1] = r
+	}
+	return string(output_runes)
 }
